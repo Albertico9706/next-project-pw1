1cfec1e83a427e9ac4e1ca46384f46b438371674
albertico9706 <abcelkuqui@gmail.com>
2024-12-05 14:19:40 -0500

    format(CardJob)

--- src/app/works/CardJob.tsx
+++ src/app/works/CardJob.tsx
@@ -10,10 +10,8 @@ export default function CardJob({job}:Props){
     return(
         <div className="p-8 bg-white shadow card dark:bg-slate-900">
             <CardJobTitle job={job}/>
-            
             <div className="card-body">
             <p>{excerpt}</p>
-            
             {job.annualSalaryMin&& <p className="tooltip" data-tip="hello">Salario:<span>{job.annualSalaryMin}</span>-<span>{job.annualSalaryMax}</span></p> }
             {job.salaryCurrency&& <p>{job.salaryCurrency}</p>}
             </div>
@@ -30,15 +28,13 @@ export function CardJobTitle({job}:Props){
     return(
         <div className="card-title  flex justify-between">
                 <div className="flex gap-4 items-center">
-                <picture>
-                {job.companyLogo&&<Image width="40" height={40} alt="Logo de la compañía" title="Logo de la compañía" src={job.companyLogo} />}
-                
-                </picture>
-                <div><h6>{job.companyName}</h6>
+                    <picture>
+                    {job.companyLogo&&<Image width="40" height={40} alt="Logo de la compañía" title="Logo de la compañía" src={job.companyLogo} />}
+                    </picture>
+                    <div><h6>{job.companyName}</h6>
                     <h6 ><b>{job.jobTitle}</b></h6></div>
                 </div>
-                
-                    <span className="flex gap-2"><CalenDate/>{job.pubDate}</span>
+                <span className="flex gap-2"><CalenDate/>{job.pubDate}</span>
             </div>
     )
 }
