1da6badd569b887cf431346d9ca03687b9a66980
albertico9706 <abcelkuqui@gmail.com>
2025-01-13 11:17:00 -0500

    fix(prisma.ts) remove adapter-pg

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
-import {PrismaPg} from "@prisma/adapter-pg"
-import {Pool} from "pg"
+/* import {PrismaPg} from "@prisma/adapter-pg"
+import {Pool} from "pg" */
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -9,13 +9,14 @@ import {Pool} from "pg"
 // https://pris.ly/d/help/next-js-best-practices
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-const connectionString= `${process.env.DATABASE_URL}`
+//remove the driver adapter temporally for debug problems
+/* const connectionString= `${process.env.DATABASE_URL}`
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter }) */
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
